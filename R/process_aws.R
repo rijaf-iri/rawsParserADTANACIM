fed19dcb2ef4d61fd3e696b2fb21ddd09058a725
Rija Faniriantsoa <rijaf@iri.columbia.edu>
2022-12-02 13:46:11 +0000

    process pulsonic data

--- R/process_aws.R
+++ R/process_aws.R
@@ -25,6 +25,33 @@ process.adcon <- function(dirAWS, dirUP){
     return(0)
 }
 
+#' Process ADCON data.
+#'
+#' Get the data from ADCON database, parse and convert into ADT format.
+#' 
+#' @param dirAWS full path to the directory containing the AWS_DATA folder.\cr
+#' @param dirPLUSO full path to the directory PULSONIC folder on ADT server.
+#' @param initData logical, if \code{TRUE} the initialization data will be processed.
+#' 
+#' @export
 
-# dirAWS <- "E:/ADT"
-# dirUP <- "/home/adt/ADT"
+process.pulsonic <- function(dirAWS, dirPLUSO, initData = FALSE){
+    dirLOG <- file.path(dirAWS, "AWS_DATA", "LOG", "PULSONIC")
+    if(!dir.exists(dirLOG))
+        dir.create(dirLOG, showWarnings = FALSE, recursive = TRUE)
+    logPROC <- file.path(dirLOG, "processing_pulsonic.txt")
+
+    ret <- try(get.pulsonic.data(dirAWS, dirPLUSO, initData), silent = TRUE)
+    if(inherits(ret, "try-error")){ 
+        mserr <- gsub('[\r\n]', '', ret[1])
+        msg <- "Getting PULSONIC data failed"
+        format.out.msg(paste(mserr, '\n', msg), logPROC)
+        return(2)
+    }
+
+    return(0)
+}
+
+# dirAWS <- "/home/adt/ADT"
+# dirPLUSO <- "/home/adt/PULSONIC"
+
